884684294837dbb266f2c57e1a85ab4c4d91cf57
Abbeythetechguy <abbeyezeilo323@gmail.com>
2023-08-28 18:03:54 +0100

    Modified changes

---
+++ index.js
@@ -0,0 +1,5 @@
+// This is for the Hamburger Menu
+function showSideNav(){
+    let displaySideNav = document.getElementById("mobile-side-nav");
+    displaySideNav.classList.toggle("showMobNav")
+}
