999126b609a7976bab6e99afbf8b8ed557657b85
Abbeythetechguy <abbeyezeilo323@gmail.com>
2023-10-11 20:36:33 +0100

    Modified Changes

--- index.js
+++ index.js
@@ -1,5 +1,36 @@
 // This is for the Hamburger Menu
-function showSideNav(){
-    let displaySideNav = document.getElementById("mobile-side-nav");
-    displaySideNav.classList.toggle("showMobNav")
+let displaySideNav = document.getElementById("mobile-side-nav");
+let x = window.matchMedia("(max-width: 700px)")
+
+function showSideNav() {
+  displaySideNav.classList.add('open')
+}
+function closeSideNav() {
+  displaySideNav.classList.remove('open')
+}
+window.onclick = function (event){
+  if (event.target == displaySideNav){
+    displaySideNav.classList.remove('open')
+  }
+}
+
+let hangingMenu = document.getElementById("hangingMenu");
+
+// hangingMenu.addEventListener('scroll', showHangingMenu);
+// hangingMenu.style.display = "none";
+
+function showHangingMenu() {
+  hangingMenu.style.display = "flex";
+}
+
+const hangingLinks = document.getElementById('.hanging-menu-links')
+hangingLinks.addEventListener("click", activeHangingLinks)
+
+let activetab =false;
+function activeHangingLinks(){
+ if (activetab) {
+    hangingLinks.document.classList.add('active')
+ } else {
+    hangingLinks.document.classList.remove('active')
+ }
 }
